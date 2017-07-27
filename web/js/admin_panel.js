e0ff7462995b557519753cf8144fdd94091793d6
Sébastien CAPPELAERE <sebcappelaere@sfr.fr>
2017-07-27 16:57:52 +0200

    Ajout modif du crédit

--- web/js/admin_panel.js
+++ web/js/admin_panel.js
@@ -3,14 +3,21 @@
  */
 $(document).ready(function () {
     $("#role-form").hide();
+    $("#credit-form").hide();
 
     $("#table-users").on('click', 'tr', function () {
         var email = $(this).find("[id='email']");
 
         $("#role-form").show();
 
+        $("#credit-form").show();
+
         $("#role-form-title").html(email.html());
 
+        $("#role-credit-title").html(email.html());
+
         $("#email-hidden").val(email.html());
+
+        $("#emailCredit-hidden").val(email.html());
     });
 });
